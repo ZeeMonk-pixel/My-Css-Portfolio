75ab2bd949e5e50978c682ccd0fda279b6fe3af1
ZeeMonk-pixel <theyoungachiever18@yahoo.com>
2022-09-18 03:55:27 +0100

    hamburger menu

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+
+var hamburger = document.querySelector('.hamburger-menu')
+var mobilelinks = document.querySelector('.mobile-link-holder')
+
+
+
+function Addclass() {
+    hamburger.classList.toggle('show')
+    mobilelinks.classList.toggle('showlinks')
+    
+}
